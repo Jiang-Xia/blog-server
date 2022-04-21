37774d3722828f13297628be7c57554a5359a97d
jiangxia <963798512@qq.com>
2022-04-21 19:51:57 +0800

    修改数据库密码注释

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,10 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { getLocalIP } from 'src/utils';
 const env = process.env.NODE_ENV;
+/*
+ * mysql8修改密码  alter user  user() identified by 'jiang123!!';
+ * https://blog.csdn.net/qq_42618394/article/details/103181778
+ */
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: env === 'production' ? '42.192.145.236' : 'localhost',

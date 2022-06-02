b6403e6dc5b189f0495946b3c20082fd6c4e1ea6
jiang-xia <963798512@qq.com>
2022-06-02 17:16:49 +0800

    feature：评论模块初步

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { RolesGuard } from '../auth/roles.guard';
 import { CommentService } from './comment.service';
@@ -10,4 +10,22 @@ import { CommentService } from './comment.service';
 @UseGuards(RolesGuard)
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
+
+  // 创建评论
+  @Post('create')
+  async create() {
+    return await this.commentService.create();
+  }
+
+  // 刪除评论
+  @Post('delete')
+  async delete() {
+    return await this.commentService.delete();
+  }
+
+  // 获取对应文章所有评论
+  @Get('findAll')
+  async getArticleComments(@Query('articleId') id) {
+    return await this.commentService.findAll(id);
+  }
 }
--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -8,4 +8,12 @@ export class CommentService {
     @InjectRepository(Comment)
     private readonly commentRepository: Repository<Comment>,
   ) {}
+  async create() {}
+  async delete() {}
+  async findAll(id: string) {
+    const qb = this.commentRepository.createQueryBuilder('comment');
+    qb.where('comment.articleId = :id', { id });
+    const data = await qb.getOne();
+    return data;
+  }
 }

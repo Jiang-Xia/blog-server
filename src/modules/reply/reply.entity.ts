b15943a581eb201e7ef2f425634af5f678c2e782
jiangxia <963798512@qq.com>
2022-08-26 23:13:12 +0800

    fix:  回复表增加创建和更新时间

--- src/modules/reply/reply.entity.ts
+++ src/modules/reply/reply.entity.ts
@@ -1,10 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Reply {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+  // 数据库 创建时间
+  @CreateDateColumn()
+  createTime: Date;
+
+  // 数据库 更新时间
+  @UpdateDateColumn()
+  updateTime: Date;
 
   @ApiProperty({ description: '评论id(父级id)' })
   @Column()

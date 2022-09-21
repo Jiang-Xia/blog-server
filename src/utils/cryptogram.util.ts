e56f81e1776bbed0e1b38c126bc558c8b3ec2d63
jiangxia <963798512@qq.com>
2022-09-21 21:28:19 +0800

    feat: 称加解密方法改进

--- src/utils/cryptogram.util.ts
+++ src/utils/cryptogram.util.ts
@@ -27,43 +27,51 @@ export function encryptPassword(password: string, salt: string): string {
 /* 
   对称加解密 和home端对应的aes加解密
 */
+// 加密密钥（长度必须是 16 的整数倍，此处为 32 位）
 const secretKey = '54050000778e380000fe5a120000b4ce';
 // 偏移量
-const iv = 'jiang-xia';
-
-export function aseEncryptParams(word: any, key = secretKey, offset = iv) {
+const iv = 'jiangxia';
+/**
+ * AES加密
+ * @description 使用加密秘钥，对 需要加密的参数 进行加密
+ * @param {string} word - 需要加密的参数
+ * @param {string} key - 加密密钥（长度必须是 16 的整数倍）
+ * @param {string} offset - 偏移量
+ * @return 16进制字符串 256位
+ */
+export function aesEncrypt(word: any, key = secretKey, offset = iv) {
   // 未加密的参数 - 从 UTF-8编码 解析出原始字符串
   const wordUTF8 = enc.Utf8.parse(word);
   // 密钥 - 从 UTF-8编码 解析出原始字符串
   const keyUTF8 = enc.Utf8.parse(key);
-  // 偏移量（在此公司内是固定的） - 从 UTF-8编码 解析出原始字符串
+  // 偏移量 从 UTF-8编码 解析出原始字符串
   const offsetUTF8 = enc.Utf8.parse(offset);
-
-  // 补充
-  // 把字符串转成 UTF-8编码 —— enc.Utf8.stringify(word);
 
   const encrypted = AES.encrypt(wordUTF8, keyUTF8, {
     iv: offsetUTF8,
     mode: mode.CBC,
     padding: pad.Pkcs7,
   });
-
-  return encrypted.toString();
+  // 转成16进制 变成大写不影响解密
+  return encrypted.toString(CryptoJS.format.Hex).toUpperCase();
 }
 
 /**
- * ASE解密
+ * AES解密
  * @description 使用加密秘钥，对 需要解密的参数 进行解密
  * @param {string} encryptedWord - 需要解密的参数
  * @param {string} key - 加密密钥（长度必须是 16 的整数倍）
  * @param {string} offset - 偏移量
+ * @return utf8 字符串
  */
-export function aesDecryptParams(encryptedWord: any, key = secretKey, offset = iv) {
+export function aesDecrypt(encryptedWord: any, key = secretKey, offset = iv) {
   // 密钥 - 从 UTF-8编码 解析出原始字符串
   const keyUTF8 = enc.Utf8.parse(key);
-  // 偏移量（在此公司内是固定的） - 从 UTF-8编码 解析出原始字符串
+  // 偏移量 从 UTF-8编码 解析出原始字符串
   const offsetUTF8 = enc.Utf8.parse(offset);
-
+  // 解析十六进制字符串
+  encryptedWord = CryptoJS.format.Hex.parse(encryptedWord);
+  // console.log('encryptedWord:',encryptedWord)
   const bytes = AES.decrypt(encryptedWord, keyUTF8, {
     iv: offsetUTF8,
     mode: mode.CBC,

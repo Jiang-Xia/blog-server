2df4a9e1103541ddd1684d8cc4568bc2887fa293
jiang-xia <963798512@qq.com>
2024-03-25 17:06:53 +0800

    feat: test

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,6 +11,22 @@ export class AppService {
       res.redirect('https://www.bing.com');
     } else if (type === 'google') {
       res.redirect('https://www.google.com');
+    } else if (type === 'html') {
+      setTimeout(() => {
+        const htmlContent = `
+          <html>
+            <head>
+              <title>Sample HTML Document</title>
+            </head>
+            <body>
+              <h1>Hello, NestJS!</h1>
+              <p>This is a sample HTML document returned by NestJS.</p>
+            </body>
+          </html>
+        `;
+        res.setHeader('Content-Type', 'text/html');
+        res.send(htmlContent);
+      }, 1500);
     } else {
       res.redirect('https://www.baidu.com');
     }
